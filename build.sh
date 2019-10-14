559a6f9fa4d600b5f67cfe9c355297e56a083809
Yecheng Fu <cofyc.jackson@gmail.com>
2019-10-14 15:33:14 +0800

    use travis install github action because the later does not support private environments

--- build.sh
+++ build.sh
@@ -4,10 +4,19 @@ declare -A config
 config["master"]="latest"
 config["0.1.3"]="0.1.3"
 
+if [ -n "$PUSH" ]; then
+  if [ -n "$DOCKER_USERNAME" -a -n "$DOCKER_PASSWORD" ]; then
+    echo "info: DOCKER_USERNAME & DOCKER_PASSWORD are both set"
+    docker login --username $DOCKER_USERNAME --password-stdin <<<"$DOCKER_PASSWORD"
+  fi
+fi
+
 for ref in ${!config[*]}; do
-    tag=${config[$ref]}
-    image=cofyc/dsvpn:$tag
-    echo "info: build $ref as image $image"
-    docker build --no-cache -t $image --build-arg GIT_REF=$ref .
+  tag=${config[$ref]}
+  image=cofyc/dsvpn:$tag
+  echo "info: build $ref as image $image"
+  docker build --no-cache -t $image --build-arg GIT_REF=$ref .
+  if [ -n "$PUSH" ]; then
     docker push $image
+  fi
 done
